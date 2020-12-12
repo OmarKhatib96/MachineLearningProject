6df7a14e68486da431bb24ca53103dbdde7739c6
Khatib Omar <khatibomar3@gmail.com>
2020-12-12 08:20:08 +0100

    PCA sklearn vs iterative svd results added

--- iterative_svd.py
+++ iterative_svd.py
@@ -98,6 +98,8 @@ def iterative_svd(X,nbr_components):
             element= iteration(A,index,first_component)
             if index==0:
                 first_component=element
+            else:
+                first_component=v_i[index-1]
             
             #print("element=",element)
             v_i.append(element)
@@ -157,21 +159,48 @@ def data_scaling(data):
 #I-Data scaling
 standardized_data=data_scaling(data)
 sample_data = standardized_data
+'''
+#II-Principal components computation
+components=iterative_svd(sample_data,4)
+print(components.shape)
+#III-projection of original data on the PC
+projection_coordinates=project_data_on_components(components[0:2])
+#IV-Data Visualization of projection
+data_visualize(projection_coordinates)
+
+'''
+#comparaison avec sklearn 
+
+# initializing the pca
+from sklearn import decomposition
+pca = decomposition.PCA()
+
+# configuring the parameteres
+# the number of components = 2
+pca.n_components = 4
+print(sample_data.shape)
+print(standardized_data)
+pca_data = pca.fit_transform(standardized_data)
+# pca_reduced will contain the 2-d projects of simple data
+print("shape of pca_reduced.shape = ", pca_data.shape)
+
+
+# attaching the label for each 2-d data point 
+pca_data = np.vstack((pca_data[:,1:3].T, labels)).T
+import seaborn as sn
+# creating a new data fram which help us in ploting the result data
+pca_df = pd.DataFrame(data=pca_data, columns=("1st_principal", "2nd_principal", "label"))
+sn.FacetGrid(pca_df, hue="label", size=6).map(plt.scatter, '1st_principal', '2nd_principal').add_legend()
+plt.show()
+
+
+
 
 #II-Principal components computation
 components=iterative_svd(sample_data,2)
 print(components.shape)
 #III-projection of original data on the PC
 projection_coordinates=project_data_on_components(components)
-
 #IV-Data Visualization of projection
 data_visualize(projection_coordinates)
 
-
-#comparaison avec sklearn 
-'''    
-from sklearn.decomposition import PCA
-pca=PCA(2)
-pca.fit(A)
-print(pca.components_)
-'''
